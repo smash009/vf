10ff9e88f54265c3126564adea48755aa4158892
smash009 <smash009@naver.com>
2020-08-09 21:17:21 +0900

    init

--- src/views/site/footer.vue
+++ src/views/site/footer.vue
@@ -1,7 +1,7 @@
 <template>
     <v-footer app color="primary" dark absolute :footer="footer">
         <v-spacer></v-spacer>
-        <div>&copy; {{ new Date().getFullYear() + '' + footer }}</div>
+        <div>&copy; {{ new Date().getFullYear() + ' ' + footer }}</div>
     </v-footer>
 </template>
 <script>
---
+++ src/views/site/menu.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+        <v-list-item-content>
+            {{ item.title }}
+        </v-list-item-content>
+    </v-list-item>
+</template>
+<script>
+export default {
+  props: ['footer'],
+  data () {
+    return {
+      items: [
+        {
+          title: 'home',
+          to: '/'
+        },
+        {
+          title: 'about',
+          to: '/about'
+        }
+      ]
+    }
+  }
+}
+</script>
