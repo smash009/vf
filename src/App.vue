034a56aeb4bf61d8758246191e6324c2fd10f0e1
smash009 <smash009@naver.com>
2020-08-09 13:52:55 +0900

    init

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,25 @@
       color="primary"
       dark
     >
-      <v-app-bar-nav-icon/>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
       <v-toolbar-title>Page title</v-toolbar-title>
     <v-spacer/>
     <v-btn icon to="/about">
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
     </v-app-bar>
+      <v-navigation-drawer v-model="drawer">
+        test
+      </v-navigation-drawer>
+    <v-content>
+      <router-view>
+
+      </router-view>
+    </v-content>
+      <v-footer app color="primary" dark absolute>
+        <v-spacer></v-spacer>
+        <div>&copy; {{ new Date().getFullYear() }}</div>
+      </v-footer>
   </v-app>
 </template>
 
